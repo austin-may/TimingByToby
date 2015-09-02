7d93a04d3da2b22e25f2c35f92a230cabd16a193
unknown <tj02180@CSCT2552315X001.ad.georgiasouthern.edu>
2015-09-02 14:04:09 -0400

    inital abstract for timing devices

---
+++ Source/TimingDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimingForToby
+{
+    abstract class TimingDevice
+    {
+        private List<Object> listeners=new List<object>();
+        public List<Tuple<TimeSpan, String>> buffer= new List<Tuple<TimeSpan,string>>(); 
+        public void addListener(Object obj);
+        abstract void addToBuffer(TimeSpan time, String bib);
+        public void clearBuffer() {
+            buffer.Clear();
+        }
+    }
+}
