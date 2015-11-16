899eb223c51624e45a1fc066ad4b5d2e6b3c06a8
Thomas Jones <tj02180@georgiasouthern.edu>
2015-11-15 19:21:18 -0500

    renameing

--- Source/TimingDevice.cs
+++ Source/TimingDevice.cs
@@ -8,36 +8,36 @@ namespace TimingForToby
 {
     public abstract class TimingDevice: IDisposable
     {
-        private int _RaceID=-1;
-        private List<Object> listeners=new List<object>();
+        private int _raceID=-1;
+        private List<Object> _listeners=new List<object>();
         public abstract void StartRace();
         public abstract void StartRace(TimeSpan ts);
         public abstract void StopRace();
         public abstract void ClearTimer();
         public abstract TimeSpan GetCurrentTime();
         public abstract void OffsetTimer(TimeSpan ts);
-        public void SetRaceID(int id) { _RaceID=id;}
+        public void SetRaceID(int id) { _raceID=id;}
         public List<Tuple<TimeSpan, String>> buffer= new List<Tuple<TimeSpan,string>>();
-        public void addListener(ITimerListener obs) { listeners.Add(obs); }
+        public void AddListener(ITimerListener obs) { _listeners.Add(obs); }
         public void Removeistener(ITimerListener obs) {
             try
             {
-                listeners.Remove(obs);
+                _listeners.Remove(obs);
             }catch(Exception e){}
         }
-        public void ClearListener(ITimerListener obs) { listeners.Clear(); }
+        public void ClearListener(ITimerListener obs) { _listeners.Clear(); }
         //public void addToBuffer(TimeSpan time, String bib);
-        public void clearBuffer() {
+        public void ClearBuffer() {
             buffer.Clear();
         }
         public void RecordTime(string bib, string time)
         {
-            if (_RaceID < 0)
+            if (_raceID < 0)
             {
                 throw new Exception("No Race set!");
             }
-            CommonSQL.AddTimeAndBib(_RaceID, bib, time);
-            foreach(ITimerListener obs in listeners){
+            CommonSQL.AddTimeAndBib(_raceID, bib, time);
+            foreach(ITimerListener obs in _listeners){
                 obs.OnTime();
             }
         }
