98e332b9d02fd077a3771546f14757870330dbdc
austinmay <am09095@georgiasouthern.edu>
2015-11-16 15:11:06 -0500

    something minor that it's making me commit

--- TimingByTobyTests/UnitTests.cs
+++ TimingByTobyTests/UnitTests.cs
@@ -9,22 +9,46 @@ namespace TimingByTobyTests
     [TestClass]
     public class UnitTests
     {
+
         [TestMethod]
         public void TestKeyboard()
         {
-            KeybordTimer keyboard = new KeybordTimer(Keys.F1.ToString());
-            keyboard.StartRace();
+            TimeSpan ts = new TimeSpan();
+            KeybordTimer keyboard = new KeybordTimer("F1");
+            Assert.IsNotNull(keyboard.GetCurrentTime());
         }
 
         [TestMethod]
         public void TestImportingRunners()
         {
-            DateTime time = DateTime.Now;
-            CommonSQL.AddRunner("4343", "2323", time, "hey", "female", "ok", "ok", "Hahaha", "Data Source=MyDatabase.sqlite;Version=3;");
-            CommonSQL.AddRunner("Bob", "Martin", time, "345", "male", "ok", "ok", "Race 3", "Data Source=MyDatabase.sqlite;Version=3;");
-            RaceData rd = new RaceData(null, "Sample Data", "Data Source=MyDatabase.sqlite;Version=3;");
-            MainWindow main = new MainWindow(rd);
-            main.OnTime();
+            //DateTime time = DateTime.Now;
+            //CommonSQL.AddRunner("4343", "2323", time, "hey", "female", "ok", "ok", "Hahaha", "Data Source=MyDatabase.sqlite;Version=3;");
+            //CommonSQL.AddRunner("Bob", "Martin", time, "345", "male", "ok", "ok", "Race 3", "Data Source=MyDatabase.sqlite;Version=3;");
+            //RaceData rd = new RaceData(null, "Sample Data", "Data Source=MyDatabase.sqlite;Version=3;");
+            //MainWindow main = new MainWindow(rd);
+            //main.OnTime();
+        }
+
+        [TestMethod]
+        public void TestTobyTimer()
+        {
+            //Test public constructor with no parameters
+            TobyTimer tobyTimer = new TobyTimer();
+            Assert.IsNotNull(tobyTimer.Elapsed());
+            //Test public constructor when taking in a datetime parameter
+            DateTime dt = new DateTime();
+            TobyTimer tobyTimer1 = new TobyTimer(dt);
+            Assert.IsNotNull(tobyTimer1.Elapsed());
+        }
+
+
+        [TestMethod]
+        public void TestFilters()
+        {
+            RaceData racedata = new RaceData();
+            Filter filter = new Filter(racedata);
+            filter.LoadDataTable();
+            Filter.BuildFromXML(racedata, "testFile", 5, 5);
         }
     }
 }
